9baaf31f0b884a7b4df24a369985484e5a00c411
sitihajarbhr <150003299+sitihajarbhr@users.noreply.github.com>
2023-12-19 16:55:54 +0800

    Percabangan3.py

---
+++ 231031111-SITI HAJAR-Percabangan3.py
@@ -0,0 +1,10 @@
+#pendapatan
+
+
+nama = str(input('Masukkan Nama Karyawan='))
+pendapatan = int(input('pendapatan='))
+
+if pendapatan > 1000 :
+ print ('status : Berhak')
+else :
+ print ('status : Tidak Berhak')
