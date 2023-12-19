fcf0bb558d0067e8d0556d0bcc69a63a87eb6a88
sitihajarbhr <150003299+sitihajarbhr@users.noreply.github.com>
2023-12-19 15:10:31 +0800

    Praktikum d5

---
+++ 231031111-SITI HAJAR-Praktikum-d5.py
@@ -0,0 +1,11 @@
+a = True
+i = 0
+limit = 8
+
+while a:
+    i += 1          #i = i+1
+    if i <= limit:
+        print('Selamat Bergabung',limit+1-i)
+    else:
+        print('Program berhenti di sini!!!!!!')
+        a = False
